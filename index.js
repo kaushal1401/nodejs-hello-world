91cb7017f6e8c8314835dce029592702144854d6
kaushal1401 <42976577+kaushal1401@users.noreply.github.com>
2019-12-23 00:53:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var http = require('http');
+var http = require("http");
 
-var server = http.createServer(function(request, response) {
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
-});
-
-var port = 80;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
